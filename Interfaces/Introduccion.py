150e6e59953a53f9820fb8d9c285d5125ec3e1de
Rigoberto Cerino <125434228+Cerino-rigo@users.noreply.github.com>
2023-11-02 16:01:40 -0600

    creacion de rama frontend (#2)

--- Interfaces/Introduccion.py
+++ Interfaces/Introduccion.py
@@ -1,5 +1,6 @@
 print("Hola mundo, esta es mi primer interfaz, bienvenidos")
 
+
 a = 50
 b= 56
 c = 89
@@ -9,6 +10,12 @@ def suma():
 
 suma()
 
+
+def mostrar():
+    print("Esta rama está destinada para los cambios necesarios en el frontend")
+mostrar()
+
 def imprime():
     print("Esta es la rama para desarrollar el backend")
 imprime()
+
